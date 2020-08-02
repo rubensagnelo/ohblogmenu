3265e4e7b1312f3a01431b6ee49542a26f7bf60b
Agnelo <rubensagnelo@gmail.com>
2020-08-01 21:26:31 -0300

    inclusão de um item e adequação do espaçamento entre os menus

--- src/components/ohblog-menu/ohblog-menu.tsx
+++ src/components/ohblog-menu/ohblog-menu.tsx
@@ -17,11 +17,14 @@ let arr = this.titulos.split(",");
 let titulo1=arr[0];
 let titulo2=arr[1];
 let titulo3=arr[2];
+let titulo4=arr[3];
 
 let arrurl = this.titulos.split(",");
 let url1=arrurl[0];
 let url2=arrurl[1];
 let url3=arrurl[2];
+let url4=arrurl[3];
+
 
  
     return <div class="ohblogmenu-wrapper">
@@ -56,6 +59,15 @@ let url3=arrurl[2];
     </div>
   </li>
 </ul>
+
+<ul class="ohblog-menu-root ohblog-menu ohblog-menu-horizontal" role="menu" tabindex="0"  >
+  <li class="ohblog-menu-submenu ohblog-menu-submenu-horizontal" >
+    <div class="ohblog-menu-submenu-title" aria-expanded="false" aria-haspopup="true">
+      <a class="submenu-title-wrapper" href={url4} >{titulo4}</a>
+    </div>
+  </li>
+</ul>
+
 </div>
 
 //      return <div>{titulo1}{titulo2}{titulo3}{arr.length}</div>;
